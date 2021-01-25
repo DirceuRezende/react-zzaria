83d19d6e830d5b4c86210828b1552948fc7ab70b
Dirceu de Souza Rezende <dirceu.srezende@gmail.com>
2021-01-25 16:11:43 -0300

    Adicionado eslint, editorconfig, standard no CRA e rescripts para alterar o CRA. M4#A44

--- src/App.js
+++ src/App.js
@@ -4,4 +4,4 @@ const App = () => (
   <h1>App</h1>
 )
 
-export default App;
+export default App
--- src/error.js
+++ src/error.js
@@ -1,26 +1,30 @@
 import { PureComponent } from 'react'
+import PropTypes from 'prop-types'
 
 class ErrorBoundary extends PureComponent {
     state = {
-        hasError: false
+      hasError: false
     }
 
-    /*Quando ocorrer um erro no react, ele cai aqui, como se fosse um catch do try catch.
+    /* Quando ocorrer um erro no react, ele cai aqui, como se fosse um catch do try catch.
       O error recebido é um erro padrão do JavaScript. */
     static getDerivedStateFromError (error) {
-        console.log('error getDerivedStateFromError:', error)
-        return { hasError: true }
+      console.log('error getDerivedStateFromError:', error)
+      return { hasError: true }
     }
 
     // Local para fazer o um log ou algo do gênero.
-    componentDidCatch(error, info) {
-        console.log('error:', error)
-        console.log('info:', info)
+    componentDidCatch (error, info) {
+      console.log('error:', error)
+      console.log('info:', info)
     }
 
-    render() {
-        return this.props.children(this.state.hasError)
+    render () {
+      return this.props.children(this.state.hasError)
     }
 }
 
+ErrorBoundary.propTypes = {
+  children: PropTypes.func.isRequired
+}
 export default ErrorBoundary

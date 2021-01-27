f8df58a92c37732b4a885db784478ad6622fb13c
Dirceu de Souza Rezende <dirceu.srezende@gmail.com>
2021-01-27 19:13:50 -0300

    Adicionado marcação e estilização na tela login. M4#A54

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { Button, Grid } from '@material-ui/core'
+import { ReactComponent as MainLogo } from './logo-react-zzaria.svg'
+import styled from 'styled-components'
 
 const Login = () => (
-  <h1>Login</h1>
+  <Container>
+    <Grid container justify='center' spacing={5}>
+      <Grid item>
+        <Logo />
+      </Grid>
+      <Grid item xs={12} container justify='center'>
+        <GitHubButton>Entrar com Github</GitHubButton>
+      </Grid>
+    </Grid>
+  </Container>
 )
+
+const Logo = styled(MainLogo)`
+  width: 100%;
+`
+
+const Container = styled.div`
+  padding: 20px;
+`
+
+const GitHubButton = styled(Button).attrs({
+  variant: 'contained',
+  fullWidth: true
+})`
+  && {
+    font-size: 25px;
+    max-width: 480px;
+    padding: 15px;
+    text-transform: none;
+  }
+`
 
 export default Login
